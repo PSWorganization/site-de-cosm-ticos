a1cd6ab79844ef3d0d40281bdd3c73255b647249
Cecília <145465067+cecilia-elen@users.noreply.github.com>
2024-07-27 21:35:14 -0300

    Add files via upload

---
+++ site de cosméticos/site de cosméticos/Sobre.php
@@ -0,0 +1,32 @@
+<?php
+include_once("header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nós</title>
+    <link rel="stylesheet" href="floralbeauty.css">
+</head>
+<body>
+
+    <div class="login-cadastro-container">
+        <div class="sobre-container">
+            <h2>Sobre Nós</h2>
+            <ol><h3>Quem somos nós?</h3>
+            <p>Somos uma equipe especializada em cosméticos nacionais e internacionais, comprometida em ajudá-lo(a) a adquirir seus produtos favoritos de forma acessível e eficiente.</p>
+            <h3>O que fazemos?</h3>
+            <p>Realizamos a importação e exportação de cosméticos ao redor do mundo, garantindo que eles cheguem até você no conforto da sua casa.</p>
+           <h3>O que você, visitante, pode fazer aqui?</h3>
+            <p>Nossa plataforma oferece segurança total dos dados coletados. Além de realizar suas compras, você tem a oportunidade de se cadastrar como vendedor e expandir seu próprio negócio.</p>
+        
+        </div>
+    </div>
+    
+</body>
+</html>
+<?php
+include_once("footer.php");
+?>
